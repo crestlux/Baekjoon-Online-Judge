412bd55f660ceae736a83de8e4b33396de91d199
Crestlux <hemi0213@gmail.com>
2022-05-18 22:08:30 +0900

    [Bronze I] Title: 원의 분할, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16478. 원의 분할/원의 분할.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cout << fixed; cout.precision(8);
+    double p, q, r;
+    cin >> p >> q >> r;
+    cout << p * r / q << "\n";
+    return 0;
+}
