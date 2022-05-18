de2525acaadbbf1ce0cdfd43b50fc9a5e3545ad4
Crestlux <hemi0213@gmail.com>
2022-05-18 22:08:28 +0900

    [Bronze III] Title: 작도하자! - ②, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16485. 작도하자！ － ②/작도하자！ － ②.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cout << fixed; cout.precision(10);
+    double c, b;
+    cin >> c >> b;
+    cout << c / b << "\n";
+    return 0;
+}
