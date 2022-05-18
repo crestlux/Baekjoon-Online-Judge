3f7ffa8ce0d51f853cc39b9c5d9ad7f920245bd9
Crestlux <hemi0213@gmail.com>
2022-05-18 22:06:38 +0900

    [Unrated] Title: 외계인의 침투, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/16490. 외계인의 침투/외계인의 침투.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int a, t;
+    cin >> a >> t;
+    cout << a * a - t * t << "\n";
+    return 0;
+}
