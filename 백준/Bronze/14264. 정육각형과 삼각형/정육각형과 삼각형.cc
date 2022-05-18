826a6f748de2c327f4cdffa677920f710630fe47
Crestlux <hemi0213@gmail.com>
2022-05-18 19:29:41 +0900

    [Bronze IV] Title: 정육각형과 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14264. 정육각형과 삼각형/정육각형과 삼각형.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    double L;
+    cout << fixed; cout.precision(13);
+    cin >> L;
+    cout << L * L * sqrt(3) / 4.0 << "\n";
+    return 0;
+}
