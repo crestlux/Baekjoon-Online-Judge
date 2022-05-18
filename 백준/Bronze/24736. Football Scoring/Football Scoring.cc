0f9f90a56812ef7708d00095f14ab27841fa6d14
Crestlux <hemi0213@gmail.com>
2022-05-18 19:21:26 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int a, b, c, d, e;
+    for (int i = 0; i < 2; i++) {
+        cin >> a >> b >> c >> d >> e;
+        cout << 6 * a + 3 * b + 2 * c + d + 2 * e << " ";
+    }
+    return 0;
+}
