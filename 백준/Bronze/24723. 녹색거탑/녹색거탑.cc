739c5f5ea3221e9e680004a498a6819320429cd4
Crestlux <hemi0213@gmail.com>
2022-05-18 19:33:13 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int N;
+    cin >> N;
+    cout << (1 << N) << "\n";
+    return 0;
+}
