e4c20a020f5101c20ca1a407ab1bab5ed85adfdc
Crestlux <hemi0213@gmail.com>
2022-05-18 22:03:50 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = i; j < N - 1; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << " ";
+        }
+        for (int j = i; j < N - 1; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
