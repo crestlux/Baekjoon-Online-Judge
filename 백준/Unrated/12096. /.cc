48d8e26a9ea430a6ff82dadb6364439458c7623a
Crestlux <hemi0213@gmail.com>
2022-05-20 01:08:17 +0900

    [Unrated] Title: , Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/12096. /.cc
@@ -0,0 +1,9 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cout << "02-521-0487\n";
+    return 0;
+}
