02f1531f2f92a6d27122e5cbec9db0f959fd5310
Crestlux <hemi0213@gmail.com>
2022-06-16 14:10:01 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    string s;
+    cin >> s;
+    cout << ":fan::fan::fan:\n:fan::" << s << "::fan:\n:fan::fan::fan:\n";
+    return 0;
+}
