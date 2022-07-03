61cf199c486b4689862fde86911e82c887356153
Crestlux <hemi0213@gmail.com>
2022-07-03 17:20:45 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -1,11 +1,2 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2")
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL);
-    int y;
-    cin >> y;
-    cout << "A\n";
-    return 0;
-}
+#import<ios>
+main(){putchar('A');}
