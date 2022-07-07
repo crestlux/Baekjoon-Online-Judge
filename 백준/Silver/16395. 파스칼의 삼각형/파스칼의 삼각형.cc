e2580c0465381ec1fd7439798b1609290a0cca1b
Crestlux <hemi0213@gmail.com>
2022-07-07 18:27:24 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 0 ms, Memory: 2244 KB -BaekjoonHub

--- 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.cc
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.cc
@@ -1,12 +1,2 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2")
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL);
-    cout << fixed; cout.precision(0);
-    long long n, k;
-    cin >> n >> k;
-    cout << 1 / (n * beta(n - k + 1, k)) << "\n";
-    return 0;
-}
+#include <bits/stdc++.h> 
+main(){int n,k;std::cin>>n>>k;printf("%.lf",round(1/(n*std::beta(n-k+1,k))));}
