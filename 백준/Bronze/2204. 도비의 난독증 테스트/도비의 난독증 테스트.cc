09dffb88bd75a5adc960b7305a8fec1eb95d6bf1
Crestlux <hemi0213@gmail.com>
2022-07-29 23:23:46 +0900

    [Bronze I] Title: 도비의 난독증 테스트, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Bronze/2204. 도비의 난독증 테스트/도비의 난독증 테스트.cc
@@ -0,0 +1,22 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    while (1) {
+        int n;
+        cin >> n;
+        if (!n) break;
+        vector<pair<string, string>> v(n);
+        for (auto &e1 : v) {
+            string s;
+            cin >> s;
+            e1 = { s, s };
+            transform(e1.first.begin(), e1.first.end(), e1.first.begin(), ::tolower);
+        }
+        ranges::sort(v);
+        cout << v[0].second << "\n";
+    }
+    return 0;
+}
