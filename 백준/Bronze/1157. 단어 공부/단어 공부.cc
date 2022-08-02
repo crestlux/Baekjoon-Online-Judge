0b3c12b3834f916504791cc70d27ebbbc81ba247
Crestlux <hemi0213@gmail.com>
2022-08-03 00:46:34 +0900

    [Bronze I] Title: 단어 공부, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -0,0 +1,23 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+int alph[26];
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    string s; char ret = 0;
+    cin >> s;
+    transform(s.begin(), s.end(), s.begin(), ::toupper);
+    for (const auto &e : s) alph[e - 'A']++;
+    for (int i = 0; const auto & e : alph) {
+        static int maxval = -1;
+        if (maxval < e) {
+            maxval = e;
+            ret = i + 'A';
+        }
+        else if (maxval == e) ret = '?';
+        ++i;
+    }
+    cout << ret << "\n";
+    return 0;
+}
