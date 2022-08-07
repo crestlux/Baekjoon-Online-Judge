f04df5692baeed978dbc95fed52132777fd6a568
Crestlux <hemi0213@gmail.com>
2022-08-08 00:14:25 +0900

    [Silver II] Title: A → B, Time: 8 ms, Memory: 124356 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -0,0 +1,35 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+constexpr ll MAX = 1e9 + 1;
+bitset<MAX> visited; queue<pair<ll, ll>> q;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    ll A, B, ret = -1;
+    cin >> A >> B;
+    auto bfs = [&](ll x) {
+        visited[x] = true;
+        q.push({ x, 1 });
+        while (!q.empty()) {
+            auto cur = q.front().first, cnt = q.front().second;
+            q.pop();
+            if (cur == B) {
+                ret = cnt;
+                return;
+            }
+            auto chk = [&](ll y) {
+                if (y > 0 and y <= B and !visited[y]) {
+                    visited[y] = true;
+                    q.push({ y, cnt + 1 });
+                }
+            };
+            chk(cur << 1);
+            chk(cur * 10 + 1);
+        }
+    };
+    bfs(A);
+    cout << ret << "\n";
+    return 0;
+}
