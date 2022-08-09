ee869ef3752b48a4874a998b69406cf78cb4c082
Crestlux <hemi0213@gmail.com>
2022-08-09 16:07:24 +0900

    [Bronze III] Title: 최소, 최대, Time: 108 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -4,10 +4,15 @@
 using namespace std;
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL);
-    int n;
+    constexpr int k = 1e6 + 1;
+    int n, maxval = -k, minval = k;
     cin >> n;
-    vector<int> v(n);
-    for (auto &e : v) cin >> e;
-    cout << *min_element(v.begin(), v.end()) << " " << *max_element(v.begin(), v.end()) << "\n";
+    while (n--) {
+        int num;
+        cin >> num;
+        maxval = max(maxval, num);
+        minval = min(minval, num);
+    }
+    cout << minval << " " << maxval << "\n";
     return 0;
 }
