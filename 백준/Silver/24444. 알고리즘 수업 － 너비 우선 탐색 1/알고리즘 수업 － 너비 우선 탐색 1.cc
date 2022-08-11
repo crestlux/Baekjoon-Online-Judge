fe75e3ca38b570c9368f73f386c01d4c0aeba7e3
Crestlux <hemi0213@gmail.com>
2022-08-11 20:05:37 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 96 ms, Memory: 9568 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -2,25 +2,25 @@
 #pragma GCC target("avx2")
 #include <bits/stdc++.h>
 using namespace std;
-vector<vector<int>> v;
-vector<bool> visited; vector<int> order;
+vector<vector<int>> v; vector<int> order;
+bitset<100001> visited;
 queue<int> q;
 int N, M, R;
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL);
     cin >> N >> M >> R;
-    v.resize(N + 1); visited.resize(N + 1, false); order.resize(N + 1);
+    v.resize(N + 1); order.resize(N + 1);
     for (int i = 0; i < M; ++i) {
         int p, q;
         cin >> p >> q;
-        v[p].push_back(q);
-        v[q].push_back(p);
+        v[p].emplace_back(q);
+        v[q].emplace_back(p);
     }
     for (auto &e : v)
         ranges::sort(e);
     int cnt = 1;
     auto bfs = [&](int x) {
-        q.push(x);
+        q.emplace(x);
         visited[x] = true;
         order[x] = cnt++;
         while (!q.empty()) {
@@ -30,7 +30,7 @@ int main() {
                 if (!visited[e]) {
                     visited[e] = true;
                     order[e] = cnt++;
-                    q.push(e);
+                    q.emplace(e);
                 }
             }
         }
