8dfe2146dc0699519d13890790f6aaaa5bb552e2
Crestlux <hemi0213@gmail.com>
2022-08-12 19:58:57 +0900

    [Silver I] Title: 조건에 맞는 정수의 개수, Time: 4 ms, Memory: 10620 KB -BaekjoonHub

---
+++ 백준/Silver/25421. 조건에 맞는 정수의 개수/조건에 맞는 정수의 개수.cc
@@ -0,0 +1,24 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+constexpr int mod = 987654321;
+ll dp[100001][11];
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    ll n, ret = 0;
+    cin >> n;
+    assert(n >= 1 and n <= 1e5);
+    fill(&dp[1][1], &dp[1][1] + 9, 1);
+    for (int i = 2; i <= n; ++i) {
+        for (int j = 1; j <= 9; ++j) {
+            dp[i][j] = (dp[i - 1][max(j - 2, 0)] + dp[i - 1][max(j - 1, 0)] + dp[i - 1][j] + dp[i - 1][min(j + 1, 10)] + dp[i - 1][min(j + 2, 10)]) % mod;
+        }
+    }
+    for (int i = 1; i <= 9; ++i) {
+        ret = (ret + dp[n][i] % mod) % mod;
+    }
+    cout << ret << "\n";
+    return 0;
+}
